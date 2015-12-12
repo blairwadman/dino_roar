88308943604784ead74f847f0b8f8af53b4f9ed2
blairwadman <sunnylondonuk@yahoo.co.uk>
2015-12-12 11:33:05 +0000

    Get service from the container

--- src/Controller/RoarController.php
+++ src/Controller/RoarController.php
@@ -2,18 +2,36 @@
 
 namespace Drupal\dino_roar\Controller;
 
+use Drupal\Core\Controller\ControllerBase;
 use Drupal\dino_roar\Jurassic\RoarGenerator;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
-class RoarController
+class RoarController extends ControllerBase
 {
+  private $roarGenerator;
+
+  public function __construct(RoarGenerator $roarGenerator)
+  {
+    $this->roarGenerator = $roarGenerator;
+  }
+
   public function roar($count)
   {
-    // Instantiate RoarGenerator object.
-    $roarGenerator = new RoarGenerator();
-    // Call method on object.
-    $roar = $roarGenerator->getRoar($count);
+
+    $roar = $this->roarGenerator->getRoar($count);
+
 
     return new Response($roar);
   }
+
+  public static function create(ContainerInterface $container)
+  {
+    $roarGenerator = $container->get('dino_roar.roar_generator');
+
+    // Create new instance of RoarController and return it.
+    return new static($roarGenerator);
+  }
+
+
 }
